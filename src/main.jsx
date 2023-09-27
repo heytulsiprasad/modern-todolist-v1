043f436f9b71efab415493230b6da73435a6378b
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-09-27 10:47:45 +0000

    feat: add todos

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { NotificationsProvider } from "@mantine/notifications";
+import { NotificationsProvider } from "@mantine/notifications"
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NotificationsProvider autoClose={3000}>
-      <App />
-    </NotificationsProvider>
+    <MantineProvider>
+          <NotificationsProvider autoClose={3000}>
+            <App />
+          </NotificationsProvider>
+    </MantineProvider>
   </React.StrictMode>
 );
