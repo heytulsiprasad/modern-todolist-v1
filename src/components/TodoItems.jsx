1094a7daba7bb4cf20cfbcdac463741ccfc742d0
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-09-28 05:18:36 +0000

    feat: add subscription and crud operations to TodoItems

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Checkbox } from "@mantine/core";
 import { useState, useEffect } from "react";
 import { ThumbDown } from "tabler-icons-react";
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, query, updateDoc, where, doc, onSnapshot, deleteDoc } from "firebase/firestore"
 import { db } from "../config/firebase";
 
 const Container = styled.div`
@@ -19,72 +19,75 @@ const CheckboxContainer = styled.div`
 const TodoItems = ({ show, uid }) => {
   const [todos, setTodos] = useState([]);
 
-
-    async function fetchTodos(show) {
-      const allTodos = [];
-      const todosRef = collection(db, "users", uid, "todos");
-      const todosSnap = await getDocs(todosRef)
-
-      todosSnap.forEach((doc) => {
-        allTodos.push({
-          ...doc.data(),
-          id: doc.id
-        })
-      });
-
-      return allTodos;
-    }
-
-
+  // show: all, active, completed
   useEffect(() => {
-    const doOps = async () => {
-        let res;
-        if (show === "all") {
-          res = fetchTodos();
-          console.log(res);
-        } else if (show === "active") {
-          res = fetchTodos()
-          console.log(res);
+    if (uid) {
+      const todosRef = collection(db, "users", uid, "todos");
 
-          // res = res.filter((todos) => todos.completed !== true);
-        } else {
-          res = fetchTodos()
-          console.log(res);
+      const unsubscribe = onSnapshot(todosRef, (querySnapshot) => {
+        const allTodos = [];
 
-          // res = res.filter((todos) => todos.completed);
-        }
+        querySnapshot.forEach((doc) => {
+          allTodos.push({
+            id: doc.id,
+            ...doc.data()
+          })
+        })
+
+        setTodos(allTodos);
+      })
+
+      return () => {
+        unsubscribe(); // when component unmounts
+      }
     }
-
-    doOps();
   }, [show])
 
+  const handleToggleComplete = async (todoId, status) => {
+    const todoRef = doc(db, "users", uid, "todos", todoId);
 
-  // all, active, completed
+    await updateDoc(todoRef, {
+      completed: !!status
+    })
+  }
+
+  const handleDelete = async (todoId) => {
+    const todoRef = doc(db, "users", uid, "todos", todoId);
+    await deleteDoc(todoRef);
+  }
 
   return (
     <Container>
-      {todos.map((todo) => (
-        <CheckboxContainer key={todo.id}>
-          <Checkbox
-            checked={todo.completed}
-            onChange={() => {}}
-            styles={{
-              label: {
-                textDecoration: todo.completed ? "line-through" : "none",
-              },
-            }}
-            size="md"
-            label={todo.title}
-          />
-          <ThumbDown
-            size={24}
-            strokeWidth={1}
-            color={"black"}
-            onClick={() => {}}
-            sx={{ cursor: "pointer" }}
-          />
-        </CheckboxContainer>
-      ))}
+      {todos
+        .filter((todo) =>
+          show === "active"
+            ? !todo.completed
+            : show === "completed"
+            ? todo.completed
+            : true
+        )
+        .map((todo) => (
+          <CheckboxContainer key={todo.id}>
+            <Checkbox
+              checked={todo.completed}
+              onChange={() => handleToggleComplete(todo.id, !todo.completed)}
+              styles={{
+                label: {
+                  textDecoration: todo.completed ? "line-through" : "none",
+                },
+              }}
+              size="md"
+              label={todo.title}
+            />
+            <ThumbDown
+              size={24}
+              strokeWidth={1}
+              color={"black"}
+              onClick={() => handleDelete(todo.id)}
+              style={{ cursor: "pointer" }}
+            />
+          </CheckboxContainer>
+        ))}
     </Container>
   );
 };
