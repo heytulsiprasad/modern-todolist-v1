d81623e12a44c415660abfe70e8f969e31fc5018
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-10-03 09:34:48 +0000

    fix: remove firebase credentials

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAryxiZM_1oHSy8VTdKrUf5n79ErfQhU_c",
-  authDomain: "modern-todolist.firebaseapp.com",
-  projectId: "modern-todolist",
-  storageBucket: "modern-todolist.appspot.com",
-  messagingSenderId: "674535101436",
-  appId: "1:674535101436:web:2165d671f722b236f71879",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
